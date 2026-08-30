278125b64d56df02ef5d953d1875c69002a555ba
Fideligo <mrafighalibf@gmail.com>
2026-08-30 16:06:39 +0700

    refactor(home): use dynamic FacultyLogo in review history card

--- lib/features/home/presentation/widgets/_widgets.dart
+++ lib/features/home/presentation/widgets/_widgets.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:ristek_material_component/ristek_material_component.dart';
 import 'package:ulaskelas/core/theme/_theme.dart';
+import 'package:ulaskelas/core/widgets/faculty_logo.dart';
 import 'package:ulaskelas/features/home/data/models/_models.dart';
 import 'package:ulaskelas/features/matkul/form/data/models/_models.dart';
 
